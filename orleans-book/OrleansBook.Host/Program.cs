5198d8100075a15b38c15534486b28d2621a1ad0
Francis Chung <francis.c.chung@gmail.com>
2025-08-29 17:35:02 +0900

    Initial Version of Orleans Host

---
+++ orleans-book/OrleansBook.Host/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Orleans;
+using Orleans.Hosting;
+using OrleansBook.GrainClasses;
+
+namespace OrleansBook.Host
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var host = new HostBuilder().UserOrleans(
+                builder =>
+                {
+                    builder.ConfigureApplicationParts(parts =>
+                            parts.AddApplicationPart(typeof(RobotGrain).Assembly).WithReferences())
+                        .UseLocalhostClustering();
+                })
+                .Build();
+
+            await host.StartAsync();
+
+            Console.WriterLine("Press Enter to stop the Silo ...");
+            Console.ReadLine();
+
+            await host.StopAsync();
+        }
+    }
+}
